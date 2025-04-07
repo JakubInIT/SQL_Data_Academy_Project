a19e3907c968dbfb2fa5b91ad7eeb9ef4d6df1bd
JakubTheITGuy <taclik.jakub@gmail.com>
2025-04-07 17:13:30 +0200

    Update SQL_fourth_task.sql

--- SQL_fourth_task.sql
+++ SQL_fourth_task.sql
@@ -1,14 +1,14 @@
 WITH price_growth AS (
 SELECT
 	year AS price_year,
-	round(((((avg(price_value) - LAG(avg(price_value)) OVER (ORDER BY year)) / LAG(avg(price_value)) OVER (ORDER BY year)) * 100)::NUMERIC),2) AS percentage_price_increase
+	round(((((avg(price_value) - LAG(avg(price_value)) OVER (ORDER BY year)) / LAG(avg(price_value)) OVER (ORDER BY year)) * 100)::NUMERIC), 2) AS percentage_price_increase
 FROM t_jakub_taclik_project_SQL_primary_final
 GROUP BY year
 ),
 payroll_growth AS (
 SELECT
 	year AS payroll_year,
-	round(((((avg(payroll_value) - LAG(avg(payroll_value)) OVER (ORDER BY year)) / LAG(avg(payroll_value)) OVER (ORDER BY year)) * 100)::NUMERIC),2) AS percentage_payroll_increase
+	round(((((avg(payroll_value) - LAG(avg(payroll_value)) OVER (ORDER BY year)) / LAG(avg(payroll_value)) OVER (ORDER BY year)) * 100)::NUMERIC), 2) AS percentage_payroll_increase
 FROM t_jakub_taclik_project_SQL_primary_final
 GROUP BY year
 )
@@ -17,7 +17,7 @@ SELECT
 	p.percentage_price_increase,
 	pay.payroll_year,
 	pay.percentage_payroll_increase,
-	round(((p.percentage_price_increase - pay.percentage_payroll_increase)::NUMERIC),2) AS difference
+	round(((p.percentage_price_increase - pay.percentage_payroll_increase)::NUMERIC), 2) AS difference
 FROM price_growth p
 JOIN payroll_growth pay
 	ON p.price_year = pay.payroll_year	
