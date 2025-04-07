db260f0f7cc104be4f32963f029be491c2f4f13e
JakubTheITGuy <taclik.jakub@gmail.com>
2025-04-07 17:11:38 +0200

    Update SQL_second_task.sql

--- SQL_second_task.sql
+++ SQL_second_task.sql
@@ -11,14 +11,14 @@ payrolls AS (
 		avg(CASE WHEN lower(category_name) LIKE '%mléko%' THEN price_value END) AS milk_price,
 		avg(CASE WHEN lower(category_name) LIKE '%chléb%' THEN price_value END) AS bread_price
 	FROM t_jakub_taclik_project_SQL_primary_final
-	WHERE YEAR IN (SELECT first_year FROM years) OR YEAR IN (SELECT last_year FROM years)
+	WHERE year IN (SELECT first_year FROM years) OR year IN (SELECT last_year FROM years)
     GROUP BY 
-    	YEAR
+    	year
 )
 SELECT
-	YEAR,
+	year,
 	round((average_payroll::NUMERIC),0) AS average_payroll_in_Kc,
 	round(((average_payroll / milk_price)::NUMERIC),0) AS amout_of_milk_in_liters,
 	round(((average_payroll / bread_price)::NUMERIC),0) AS amout_of_bread_in_kilograms
 FROM payrolls
-ORDER BY YEAR;
+ORDER BY year;
